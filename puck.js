ead0a1718b6bbc65f50bdaf5b87546f6e3832c80
Sarveshvarn Ramprasad <71520735+Sarveshvarn@users.noreply.github.com>
2023-05-19 15:35:17 +0530

    Update puck.js

--- puck.js
+++ puck.js
@@ -63,13 +63,11 @@ class Puck {
         }
         
         if (this.x - this.r > width) {
-            ding.play();
             leftScore++;
             this.reset();
         }
         
         if (this.x + this.r < 0) {
-            ding.play();
             rightScore++;
             this.reset();
         }
